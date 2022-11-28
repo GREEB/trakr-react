d2830ebf3d1ad52c2526f0bdfb839aae94e4e50d
greeb <gr44bli@gmail.com>
2022-11-28 14:56:17 +0100

    buffer points

--- src/components/canvas/Shader/Shader.tsx
+++ src/components/canvas/Shader/Shader.tsx
@@ -1,11 +1,13 @@
 import * as THREE from 'three'
-import { useFrame, extend } from '@react-three/fiber'
-import { useRef, useState } from 'react'
+import { useFrame, extend, useLoader } from '@react-three/fiber'
+import { useMemo, useRef, useState } from 'react'
 import { shaderMaterial } from '@react-three/drei'
+import { BufferAttribute } from "three";
 
 import vertex from './glsl/shader.vert'
 import fragment from './glsl/shader.frag'
 import { useRouter } from 'next/router'
+import circleImg from "./../../../assets/index.png";
 
 const ColorShiftMaterial = shaderMaterial(
   {
@@ -28,31 +30,56 @@ const Shader = (props) => {
   const [hovered, setHover] = useState(false)
   const router = useRouter()
 
-  useFrame((state, delta) => {
-    if (meshRef.current) {
-      meshRef.current.rotation.x = meshRef.current.rotation.y += 0.01
+  // useFrame((state, delta) => {
+  //   if (meshRef.current) {
+  //     meshRef.current.rotation.x = meshRef.current.rotation.y += 0.01
+  //   }
+  //   if (meshRef.current.material) {
+  //     meshRef.current.material.uniforms.time.value +=
+  //       Math.sin(delta / 2) * Math.cos(delta / 2)
+  //   }
+  // })
+  const CircleImg = useLoader(THREE.TextureLoader, circleImg.src);
+  const count = 10;
+  const sep = 1;
+  let positions = useMemo(() => {
+    let positions = [];
+    for (let xi = 0; xi < count; xi++) {
+      for (let zi = 0; zi < count; zi++) {
+        for (let yi = 0; yi < count; yi++) {
+          let x = sep * (xi - count / 2);
+          let z = sep * (zi - count / 2);
+          let y = sep * (yi - count / 2);
+          positions.push(x, y, z);
+
+        }
+      }
     }
-    if (meshRef.current.material) {
-      meshRef.current.material.uniforms.time.value +=
-        Math.sin(delta / 2) * Math.cos(delta / 2)
-    }
-  })
+    return new Float32Array(positions);
+  }, [count, sep]);
 
   return (
-    <mesh
-      ref={meshRef}
-      scale={hovered ? 1.1 : 1}
-      onClick={() => {
-        router.push(`/box`)
-      }}
-      onPointerOver={(e) => setHover(true)}
-      onPointerOut={(e) => setHover(false)}
-      {...props}
-    >
-      <boxBufferGeometry args={[1, 1, 1]} />
-      {/* @ts-ignore */}
-      <colorShiftMaterial key={ColorShiftMaterial.key} time={3} />
-    </mesh>
+    <points>
+      <bufferGeometry attach="geometry">
+        <bufferAttribute
+
+          attach="attributes-position"
+          array={positions}
+          count={positions.length / 3} //
+          itemSize={3}
+        />
+      </bufferGeometry>
+      <pointsMaterial
+        attach="material"
+        map={CircleImg}
+        color={0x000000}
+        sizes={0.1}
+        sizeAttenuation
+        transparent={false}
+        alphaTest={0.7}
+        opacity={0.9}
+      />
+    </points>
   )
 }
 
